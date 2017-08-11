581f0f472564b8521079d55e34a9c28f2b9518fa
Ellis Benjamin <ellis.benjamin@outlook.com>
2017-08-11 13:51:16 +0200

    add last command in console

--- src/Command/TranslatorGenerateCommand.php
+++ src/Command/TranslatorGenerateCommand.php
@@ -10,6 +10,7 @@ namespace Command;
 
 use Logic\FileFinder;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -33,6 +34,19 @@ class TranslatorGenerateCommand extends Command implements CollectionFactoryObse
     protected function execute( InputInterface $input, OutputInterface $output )
     {
         $this->output = $output;
+        $table = new Table($output);
+        $table
+            ->setHeaders(array('ISBN', 'Title', 'Author'))
+            ->setRows(array(
+                array('99921-58-10-7', 'Divine Comedy', 'Dante Alighieri'),
+                array('9971-5-0210-0', 'A Tale of Two Cities', 'Charles Dickens'),
+                array('960-425-059-0', 'The Lord of the Rings', 'J. R. R. Tolkien'),
+                array('80-902734-1-6', 'And Then There Were None', 'Agatha Christie'),
+            ))
+        ;
+        $table->render();
+        return;
+
         $path = $input->getArgument( 'path' );
         $name = $input->getArgument( 'name' );
 
